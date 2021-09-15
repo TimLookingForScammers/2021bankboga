ecfc6adb03bd6ff6aae24cb90b63774aa0aada2d
TimLookingForScammers <90778660+TimLookingForScammers@users.noreply.github.com>
2021-09-15 17:15:17 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,88 @@
+accounts = [{
+    nickname: 'Personal Checking',
+    number: '1337',
+    balance: 1240.36,
+},
+{
+    nickname: 'Standard Credit Card',
+    number: '2579',
+    balance: 2025.25,
+},
+{
+    nickname: 'High-Interest Savings',
+    number: '2580',
+    balance: 3250.36,
+},
+{
+    nickname: 'Fixed-Rate 5 Year Term CD',
+    number: '9704',
+    balance: 5000.00,
+}]
+
+transactions = [{
+    description: 'Spotify',
+    amount: -10,
+    date: new Date(2021, 4, 7),
+    account: 0
+},
+{
+    description: 'Dunkin\' Donuts',
+    amount: -25,
+    date: new Date(2021, 4, 4),
+    account: 0
+},
+{
+    description: 'Candy Crush',
+    amount: -13.37,
+    date: new Date(2021, 4, 5),
+    account: 0
+},
+{
+    description: `XFER FROM ACCT x${accounts[2].number}`,
+    amount: 250,
+    date: new Date(2021, 3, 1),
+    account: 0
+},
+{
+    description: 'Check Deposit',
+    amount: 33.10,
+    date: new Date(2021, 4, 20),
+    account: 2
+},
+{
+    description: 'Check #103 Withdrawal',
+    amount: -1392.14,
+    date: new Date(2021, 4, 16),
+    account: 0
+},
+{
+    description: 'QVC*SHOPPING/BILL',
+    amount: -140.50,
+    date: new Date(2021, 4, 10),
+    account: 0
+},
+{
+    description: 'Netflix',
+    amount: -12.94,
+    date: new Date(2021, 6, 25),
+    account: 0
+}]
+
+transfers = [{
+    origin: 2,
+    destination: 0,
+    amount: 250,
+    date: new Date(2021, 6, 1)
+},
+{
+    origin: 0,
+    destination: 3,
+    amount: 5000,
+    date: new Date(2021, 6, 22)
+}]
+
+module.exports = (() => ({
+    accounts,
+    transactions,
+    transfers
+}))()
